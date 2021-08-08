cac7ce221cb4e0f063354ac443445db647a08bd2
Jschustin <57303609+Jschustin@users.noreply.github.com>
2021-08-08 19:30:54 +0200

    Update StepperMotor.py

--- StepperMotor.py
+++ StepperMotor.py
@@ -1,3 +1,23 @@
 from machine import Pin 
 import time
 
+
+dirPin=Pin(32,Pin.OUT)
+stepPin=Pin(14,Pin.OUT)
+
+def turn(n,d):
+    dirPin(d)
+    for i in range(n):
+        stepPin(1)
+        time.sleep(0.001)
+        stepPin(0)
+        time.sleep(0.001)
+
+
+def keepturn(direction):
+    dirPin(direction)
+    while True:
+        stepPin(1)
+        time.sleep(0.001)
+        stepPin(0)
+        time.sleep(0.001)
